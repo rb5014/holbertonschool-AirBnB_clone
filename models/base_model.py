4fa5cd1fb02ef487c65b30e05e24745dc9de32aa
rb5014 <5014@holbertonschool.com>
2022-10-10 23:37:46 +0200

    task 4 update added, support of kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,14 @@ from datetime import datetime
 class BaseModel:
     '''Parent class to store data'''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''BaseModel Constructor'''
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.isoformat(datetime.now())
-        self.updated_at = self.created_at
+        if kwargs:
+            self.update(*args, **kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         '''Method to change print output of the instance'''
@@ -22,18 +24,29 @@ class BaseModel:
         return ("[{}] ({}) {}".format(self.__class__.__name__,
                                       self.id, self.__dict__))
 
-    def to_save(self):
+    def save(self):
         '''
         Public method to update public instance and store the
         change in the public instance attribute <updated_at>
         '''
 
-        self.updated_at = datetime.isoformat(datetime.now())
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         '''Used to return a dict of all attribute of the instance'''
         # Create the dict
-        dictInst = self.__dict__
+        dictInst = self.__dict__.copy()
         # Add a key '__class__' with value: the class name of the object
         dictInst['__class__'] = self.__class__.__name__
+        dictInst['created_at'] = datetime.isoformat(dictInst['created_at'])
+        dictInst['updated_at'] = datetime.isoformat(dictInst['updated_at'])
         return dictInst
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute(keys) in kwargs
+        """
+        for key, value in kwargs.items():
+            if key == 'created_at' or key == 'updated_at':
+                setattr(self, key, datetime.fromisoformat(value))
+            elif key != '__class__':
+                setattr(self, key, value)
