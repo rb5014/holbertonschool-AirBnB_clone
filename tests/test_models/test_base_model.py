737343fe158bb34e0f5e1cf679df40a37f6a6582
rb5014 <5014@holbertonschool.com>
2022-10-11 15:57:01 +0200

    chmod all

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,7 +5,6 @@
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
 from os import path
 
 
@@ -47,49 +46,3 @@ class Test_BaseModel(unittest.TestCase):
         s = f"[{self.base.__class__.__name__}] " \
             f"({self.base.id}) {self.base.__dict__}"
         self.assertEqual(self.base.__str__(), s)
-
-
-class Test_FileStorage(unittest.TestCase):
-    """Test different attributes and methods of FileStorage instances
-    """
-
-    def setUp(self):
-        """set up of the FileStorage instance
-        """
-        self.storage = FileStorage()
-
-    def test___file_path(self):
-        """test of __file_path value equal to "file.json"
-        """
-        self.assertTrue(path.exists("file.json"))
-
-    def test__objects(self):
-        """test of __objects value equal to {}
-        """
-        self.assertEqual(type(self.storage.all()), dict)
-
-    def test_all(self):
-        """test of __objects value equal to storage.all()
-        """
-        self.assertEqual(type(self.storage.all()), dict)
-
-    def test_new(self):
-        """test of new() adding new object to __objects
-        """
-        all_objs = self.storage.all().copy()
-        self.base = BaseModel()
-        all_objs2 = self.storage.all().copy()
-        self.assertNotEqual(all_objs, all_objs2)
-
-    def test_save(self):
-        """test save() creates a file
-        """
-        self.storage.save()
-        self.assertTrue(path.exists("file.json"))
-
-    def test_reload(self):
-        """test reload recreates all objects from "file.json"
-        """
-        all_objs = self.storage.all().copy()
-        for key in all_objs:
-            self.assertIsInstance(all_objs[key], BaseModel)
