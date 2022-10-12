925e293e5430e06c0a362deffbc9e602c719e6c6
rb5014 <5014@holbertonschool.com>
2022-10-12 23:49:23 +0200

    creation of a file containing all error methods, to reduce length of console file and also added handling of functions of type classname.function

--- console.py
+++ console.py
@@ -5,8 +5,8 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
-import inspect
-
+import re
+from errors import Errors_
 d = storage.all()  # avoid to write the method everytime
 
 
@@ -34,7 +34,7 @@ class HBNBCommand(cmd.Cmd):
             print ** class name missing ** (ex: $ create)
             If the class name doesn’t exist:
             print ** class doesn't exist ** (ex: $ create MyModel)'''
-        if self.error_checker("create", arg) is True:
+        if Errors_.error_checker("create", arg) is True:
             b = eval(f"{arg}()")
             b.save()
             print(b.id)
@@ -43,84 +43,74 @@ class HBNBCommand(cmd.Cmd):
         """Prints the string representation of an instance based
         on the class name and id
         """
-        if self.error_checker("show", arg) is True:
+        if Errors_.error_checker("show", arg) is True:
             tuple_args = tuple(arg.split())
             print(d[f"{tuple_args[0]}.{tuple_args[1]}"])
 
     def do_all(self, arg):
         """Prints all string representation of all instances based
-        or not on the class name
+        or not on the class name, each case with different methods
+        """
+        if not arg:
+            self.print_all_inst()
+        else:
+            self.print_cls_inst(arg)
+
+    def print_all_inst(self):
+        """CALLED BY do_all
+        Return a list of  all string representation of all instances
         """
         list_obj = []
-        if not arg:
+        for obj_id in d:
+            list_obj.append(str(d[obj_id]))
+        print(list_obj)
+
+    def print_cls_inst(self, arg):
+        """CALLED BY do_all
+        Return all string representation of all instances OF A CLASS
+        """
+        list_obj = []
+        if Errors_.error_checker("all", arg) is True:
             for obj_id in d:
-                list_obj.append(str(d[obj_id]))
+                if arg + "." in obj_id:
+                    list_obj.append(str(d[obj_id]))
             print(list_obj)
-        else:
-            if self.error_checker("all", arg) is True:
-                for obj_id in d:
-                    if arg + "." in obj_id:
-                        list_obj.append(str(d[obj_id]))
-                print(list_obj)
 
     def do_destroy(self, arg):
         l_arg = tuple(arg.split())
-        if self.error_checker("destroy", arg) is True:
+        if Errors_.error_checker("destroy", arg) is True:
             del d[f"{l_arg[0]}.{l_arg[1]}"]
 
-    def error_checker(self, cmd_name, arg):
-        """Select the error handlers corresponding
+    def do_count(self, arg):
+        """Count and print the number of instances of a class
         """
-        tuple_args = tuple(arg.split())
+        nb_instances = 0
+        if Errors_.error_checker("count", arg) is True:
+            for obj_id in d:
+                if arg + "." in obj_id:
+                    nb_instances += 1
+            print(nb_instances)
 
-        if self.class_checker(cmd_name, tuple_args) is False:
-            return False
-        if cmd_name != 'create' and cmd_name != "all":
-            if self.id_checker(tuple_args) is False:
-                return False
-        if cmd_name == 'update':
-            if self.attribute_checker(tuple_args) is False:
-                return False
-        return True
-
-    def class_checker(self, cmd_name, tuple_args):
-        """ Validate first arg if its a class
-        If cmd_name is all, only check tuple_args[0] if the arg[0] exists
-        in the list (meaning len(tuple_args) > 0)
+    def default(self, line: str) -> None:
+        """Default behavior when command prefix not recognized
+        Use function select_func to check if function is in
+        format "class.func" or "class.show('someid'), etc
         """
-        if len(tuple_args) == 0 and cmd_name != "all":
-            print("** class name missing **")
-            return False
-        if cmd_name != "all" or len(tuple_args) > 0:
-            try:
-                inspect.isclass(eval(tuple_args[0]))
-            except Exception:
-                print("** class doesn't exist **")
-                return False
-        return True
+        try:
+            args = tuple(re.split('[.()"]', line))
+            self.select_func(args)
+        except Exception:
+            return super().default(line)
 
-    def id_checker(self, tuple_args):
-        """Validate second arg if the id  matches an object
+    def select_func(self, args):
+        """Multiple try except to start functions
         """
-        if len(tuple_args) < 2:
-            print("** instance id missing **")
-            return False
-        if tuple_args[0] + "." + tuple_args[1] not in storage.all():
-            print("** no instance found **")
-            return False
-        return True
-
-    def attribute_checker(self, tuple_args):
-        """Validate third arg and fourth arg
-        if the attribute name and attribute value are valid
-        """
-        if len(tuple_args) < 3:
-            print("** attribute name missing **")
-            return False
-        if len(tuple_args) < 4:
-            print("** value missing **")
-            return False
-        return True
+        func = args[1]
+        cls = args[0]
+        if func == "all" or func == "count":
+            eval(f"self.do_{func}('{cls}')")
+        if func == "show" or func == "destroy":
+            eval(f"self.do_{func}('{cls} {args[3]}')")
 
 
 if __name__ == '__main__':
