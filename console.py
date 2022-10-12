0790977cdf7d42a76a4ce0229cf8ae44f2de8c6b
rb5014 <5014@holbertonschool.com>
2022-10-12 19:47:56 +0200

    d = storage.all added as global variable and list args changed to tuple

--- console.py
+++ console.py
@@ -7,6 +7,8 @@ from models.base_model import BaseModel
 from models import storage
 import inspect
 
+d = storage.all()  # avoid to write the method everytime
+
 
 class HBNBCommand(cmd.Cmd):
     '''Class used for command prompt'''
@@ -42,14 +44,13 @@ class HBNBCommand(cmd.Cmd):
         on the class name and id
         """
         if self.error_checker("show", arg) is True:
-            l_args = arg.split()
-            print(storage.all()[f"{l_args[0]}.{l_args[1]}"])
+            tuple_args = tuple(arg.split())
+            print(d[f"{tuple_args[0]}.{tuple_args[1]}"])
 
     def do_all(self, arg):
         """Prints all string representation of all instances based
         or not on the class name
         """
-        d = storage.all()
         list_obj = []
         if not arg:
             for obj_id in d:
@@ -61,65 +62,62 @@ class HBNBCommand(cmd.Cmd):
                     if arg + "." in obj_id:
                         list_obj.append(str(d[obj_id]))
                 print(list_obj)
-        
+
     def do_destroy(self, arg):
-        
-        d = storage.all()
-        l_arg = arg.split()
+        l_arg = tuple(arg.split())
         if self.error_checker("destroy", arg) is True:
             del d[f"{l_arg[0]}.{l_arg[1]}"]
-
 
     def error_checker(self, cmd_name, arg):
         """Select the error handlers corresponding
         """
-        l_args = arg.split()
+        tuple_args = tuple(arg.split())
 
-        if self.class_checker(cmd_name, l_args) is False:
+        if self.class_checker(cmd_name, tuple_args) is False:
             return False
         if cmd_name != 'create' and cmd_name != "all":
-            if self.id_checker(l_args) is False:
+            if self.id_checker(tuple_args) is False:
                 return False
         if cmd_name == 'update':
-            if self.attribute_checker(l_args) is False:
+            if self.attribute_checker(tuple_args) is False:
                 return False
         return True
 
-    def class_checker(self, cmd_name, l_args):
+    def class_checker(self, cmd_name, tuple_args):
         """ Validate first arg if its a class
-        If cmd_name is all, only check l_args[0] if the arg[0] exists
-        in the list (meaning len(l_args) > 0)
+        If cmd_name is all, only check tuple_args[0] if the arg[0] exists
+        in the list (meaning len(tuple_args) > 0)
         """
-        if len(l_args) == 0 and cmd_name != "all":
+        if len(tuple_args) == 0 and cmd_name != "all":
             print("** class name missing **")
             return False
-        if cmd_name != "all" or len(l_args) > 0:
+        if cmd_name != "all" or len(tuple_args) > 0:
             try:
-                inspect.isclass(eval(l_args[0]))
+                inspect.isclass(eval(tuple_args[0]))
             except Exception:
                 print("** class doesn't exist **")
                 return False
         return True
 
-    def id_checker(self, l_args):
+    def id_checker(self, tuple_args):
         """Validate second arg if the id  matches an object
         """
-        if len(l_args) < 2:
+        if len(tuple_args) < 2:
             print("** instance id missing **")
             return False
-        if l_args[0] + "." + l_args[1] not in storage.all():
+        if tuple_args[0] + "." + tuple_args[1] not in storage.all():
             print("** no instance found **")
             return False
         return True
 
-    def attribute_checker(self, l_args):
+    def attribute_checker(self, tuple_args):
         """Validate third arg and fourth arg
         if the attribute name and attribute value are valid
         """
-        if len(l_args) < 3:
+        if len(tuple_args) < 3:
             print("** attribute name missing **")
             return False
-        if len(l_args) < 4:
+        if len(tuple_args) < 4:
             print("** value missing **")
             return False
         return True
