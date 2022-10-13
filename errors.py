5e3dc87040fdbed25faf664e49995c9aab89223a
root <you@example.com>
2022-10-13 07:10:25 -0700

    add state in console

--- errors.py
+++ errors.py
@@ -8,6 +8,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from models.state import State
 import inspect
 d = storage.all()
 
