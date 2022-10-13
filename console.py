e1c0dbe25d08571b4c2ed2f8d84f73dad4336fee
root <you@example.com>
2022-10-13 06:36:39 -0700

    add import state in console.py

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from models.state import State
 from models import storage
 import re
 from errors import Errors_
