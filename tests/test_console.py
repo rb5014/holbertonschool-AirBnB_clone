1250217e37ea184f3de843a3df930f96d4722beb
rb5014 <5014@holbertonschool.com>
2022-10-14 17:38:42 +0200

    test_console created

---
+++ tests/test_console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Unittest module of console.py
+"""
+import unittest
+from console import HBNBCommand
+
+
+class Test_Console(unittest.TestCase):
+    """Test of the console commands
+    """
+
+    def start_console(self):
+        """Starts the console
+        """
+        
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
