9a9dae092b8579c88f88350904e1771bdfc10fb4
rb5014 <5014@holbertonschool.com>
2022-10-17 00:11:17 +0200

    fixed bug class Place name changed

--- console.py
+++ console.py
@@ -7,7 +7,7 @@ from models.base_model import BaseModel
 from models.user import User
 from models.city import City
 from models.amenity import Amenity
-from models.place import pl
+from models.place import Place
 from models.review import Review
 from models.state import State
 from models import storage
